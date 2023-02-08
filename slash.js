6c61dd297295989d6c5bb3a043de99e4d2ab47b2
AnonCYTO <anoncyto776@gmail.com>
2023-02-08 18:12:32 +0530

    runtime changes

--- slash.js
+++ slash.js
@@ -1,36 +1,38 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId } = require('./config.json');
+const { REST, Routes } = require('discord.js')
+const { clientId, guildId } = require('./config.json')
 const token = process.env.token
-const fs = require('node:fs');
+const fs = require('node:fs')
+require('dotenv').config()
 
-const commands = [];
+const commands = []
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'))
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+  const command = require(`./commands/${file}`)
+  commands.push(command.data.toJSON())
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token)
 
 // and deploy your commands!
-(async () => {
+;(async () => {
   try {
-    console.log(`Refreshing ${commands.length} application commands...`);
+    console.log(`Refreshing ${commands.length} application commands...`)
 
     // The put method is used to fully refresh all commands with the current set
-    const data = await rest.put(
-      Routes.applicationCommands(clientId),
-      { body: commands },
-    );
+    const data = await rest.put(Routes.applicationCommands(clientId), {
+      body: commands,
+    })
 
-    console.log(`Reloaded ${commands.length} commands.`);
-    console.log("---------------------------------------");
+    console.log(`Reloaded ${commands.length} commands.`)
+    console.log('---------------------------------------')
   } catch (error) {
     // And of course, make sure you catch and log any errors!
-    console.error(error);
+    console.error(error)
   }
-})();
+})()
