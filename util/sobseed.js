a5a51774a0db36f7ca2d366e03e53f92583ef8be
AnonCYTO <anoncyto776@gmail.com>
2023-02-08 18:17:29 +0530

    manual update to 2.1.1 :sob:

--- util/sobseed.js
+++ util/sobseed.js
@@ -1,14 +1,12 @@
-const lts = "v2.1";
-const versions = [
-  "v1.0",
-  "v2.0",
-  "v2.1"
-]
+const lts = 'v2.1.1'
+const versions = ['v1.0', 'v2.0', 'v2.1', 'v2.1.1']
 const getVer = {
-  details: require('./so.json').desc || "failed to get desc",
-  changes: require('./so.json').changes || "failed to get changes",
-  download: require('./so.json').download || "https://failed.to.fetch.details"
+  details: require('./so.json').desc || 'failed to get desc',
+  changes: require('./so.json').changes || 'failed to get changes',
+  download: require('./so.json').download || 'https://failed.to.fetch.details',
 }
 module.exports = {
-  lts, getVer, versions
+  lts,
+  getVer,
+  versions,
 }
