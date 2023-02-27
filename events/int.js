babc761118abcfb713d3698b3c4e948f0ae9dd3e
rrryfu <83335294+mangorifo@users.noreply.github.com>
2023-02-27 09:03:04 +0800

    Update int.js

--- events/int.js
+++ events/int.js
@@ -5,10 +5,9 @@ module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
     try {
-      if (!interaction.isChatInputCommand()) return;
       const command = interaction.client.commands.get(interaction.commandName);
       if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
+        console.log(`No command matching ${interaction.commandName} was found.`);
         return;
       }
       await command.execute(interaction);
